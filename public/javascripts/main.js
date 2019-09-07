605956bf6f1b14b0a288328f61a6cd3958044933
JaeHyeonKim19 <JaeHyeonKim19@gmail.com>
2019-09-08 00:54:28 +0900

    Refact: Change main page to client side rendering

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,3 +1,6 @@
 import {signInHandler} from './signIn/signIn.js';
+import {mainHtml} from './mainHtml.js';
+
+document.body.innerHTML = mainHtml;
 
 signInHandler();
---
+++ public/javascripts/mainHtml.js
@@ -0,0 +1,17 @@
+const mainHtml = /* html */ `
+<div class="wrapper">
+    <header>
+        <div class="logo"></div>
+    </header>
+    <div class="content">
+        <div class="main_buttons_wrapper">
+            <button type="button" class="main_sign_in_button">로그인하기</button>
+        </div>
+    </div>
+    <footer>
+        <div class="footer_content"></div>
+    </footer>
+</div>
+`;
+
+export {mainHtml};
